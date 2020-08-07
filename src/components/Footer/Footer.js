79306954969b7616f37975ff41cff0bb9febfb8e
EyePatch <007abeershariyar@gmail.com>
2020-08-07 21:55:40 +0600

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 class Footer extends Component {
-  render() {
-    return (
-      <div className="row" id="footer">
-        <div className="medium-12 columns">
-          <p>
-            Copyright 2019 ,{" "}
-            <a href="https://fahmidasclassroom.com"> fahmidasclassroom</a>
-          </p>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="row" id="footer">
+				<div className="medium-12 columns">
+					<p>
+						Copyright 2020 ,{" "}
+						<a href="https://shariyarabeer.github.io/demoapp/?fbclid=IwAR31TMmjhxPq0Kt6hn48zqS7pYoh-Qf2V01ijHdfHa-Pl3GxvF1j84escPg#/react-auth-ui/">
+							{" "}
+							Shariyar Abeer
+						</a>
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
 export default Footer;
